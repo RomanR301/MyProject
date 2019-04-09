1cd55aa72aa13faf38e02c4a2fb2b7f60b228a80
Roman Savchyk <roman.sav15@gmail.com>
2019-04-09 15:12:14 +0000

    modal windows in gallery

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// const galeryModal = document.getElementById('myModal');
+// const galeryImg = document.querySelectorAll('.portfolio-img');
+// const modalImg = document.getElementById('img01');
+// const captionText = document.getElementById('caption');
+
+// galeryImg.onclick = function(){
+//   galeryModal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// const span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function() { 
+//   galeryModal.style.display = "none";
+// } 
+// ЗАПИТАТИ В СЕРЕДУ
+
+
