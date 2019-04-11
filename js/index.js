7dccbd68550c5b8c566837d627fbfb0ac02998b8
Roman Savchyk <roman.sav15@gmail.com>
2019-04-11 16:33:54 +0000

    fixed gallery modal, added hover on header nav

--- js/index.js
+++ js/index.js
@@ -28,7 +28,37 @@ $(function(){
     $('.close_modal, .black_fill').click(function() {
         $('.modal_box, .black_fill').hide();
     });
+
+        
+    $('.header-nav a:nth-of-type(1)').mouseover(function(){
+        $('.nav-hover_home').css('display', 'block');
+    });
+    $('.header-nav a').mouseout(function(){
+        $('.nav-hover_home').css('display', 'none');
+    });
     
+    $('.header-nav a:nth-of-type(2)').mouseover(function(){
+        $('.nav-hover_portfolio').css('display', 'block');
+    });
+    $('.header-nav a').mouseout(function(){
+        $('.nav-hover_portfolio').css('display', 'none');
+    });
     
+    $('.header-nav a:nth-of-type(3)').mouseover(function(){
+        $('.nav-hover_blog').css('display', 'block');
+    });
+    $('.header-nav a').mouseout(function(){
+        $('.nav-hover_blog').css('display', 'none');
+    });
     
+    $('.header-nav a:nth-of-type(4)').mouseover(function(){
+        $('.nav-hover_contact').css('display', 'block');
+    });
+    $('.header-nav a').mouseout(function(){
+        $('.nav-hover_contact').css('display', 'none');
+    });
+
 });
+
+
+
